f58d01ebcd5dd2d53b570f770a48f28b6662db6d
RisDicom <risto.hirvilammi@gmail.com>
2025-08-06 11:46:43 +0300

    Update config.py
    v0.3.0 alpha päivitys

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ Sisältää mm. sovellusversion, käyttöliittymässä käytettävät emojit ja 
 sekä yleisesti tunnettujen riskielinten (OAR) nimet.
 """
 
-APP_VERSION = "v0.2.5-alpha" # <--- SOVELLUKSEN VERSIO PÄIVITETTY
+APP_VERSION = "v0.3.0-alpha" # <--- SOVELLUKSEN VERSIO PÄIVITETTY
 
 # --- Emojit ja Värit ---
 EMOJI_COPY = "📋"; EMOJI_CROP_IN = "✂️"; EMOJI_CROP_OUT = "✂️"
